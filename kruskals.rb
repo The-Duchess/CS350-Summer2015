eddc29b6d751a9003d43c2b74fc2915b95c617fc
Isaac Archer <archeri@cat.pdx.edu>
2015-08-11 04:06:57 -0700

    update

--- kruskals.rb
+++ kruskals.rb
@@ -2,14 +2,18 @@
 
 class Union_Find
       def initialize(length)
-            @vertices = (0..length).to_a
+            @vertices = {}
+      end
+
+      def add_edge(n, sp, ep)
+            @vertices.store("#{sp}_#{ep}", n.to_i)
       end
 
       def connected(v_1, v_2)
-            puts @vertices
-            puts v_1
-            puts v_2
-            if @vertices[v_1] == @vertices[v_2]
+            t_a = "#{v_1[:start]}_#{v_1[:end]}"
+            t_b = "#{v_2[:start]}_#{v_2[:end]}"
+
+            if @vertices[:t_a] == @vertices[:t_b]
                   return true
             end
 
@@ -17,8 +21,11 @@ class Union_Find
       end
 
       def union(v_1,v_2)
-            vertex_1, vertex_2 = @vertices[v_1], @vertices[v_2]
-            @vertices.map! {|i| (i == vertex_1) ? vertex_2 : i }
+            t_a = "#{v_1[:start]}_#{v_1[:end]}"
+            t_b = "#{v_2[:start]}_#{v_2[:end]}"
+
+            vertex_1, vertex_2 = @vertices[:t_a], @vertices[:t_b]
+            return @vertices.map! { |i| (i == vertex_1) ? vertex_2 : i }
       end
 end
 
@@ -26,17 +33,23 @@ file = File.readlines("city-pairs.txt")
 
 lines = file.map { |fr| fr.split("\n").map(&:to_s) }
 
+set_V = Union_Find.new(lines.length - 1)
+
 edges = []
+
+i = 0
 
 lines.each do |line|
       t_tokens = line[0].split(" ").map(&:to_s)
       t_edge = { :start => t_tokens[0], :end => t_tokens[1], :length => t_tokens[2] }
       edges.push(t_edge)
+      set_V.add_edge(i, t_edge[:start], t_edge[:end])
+      i += 1
 end
 
-edges.sort_by { |v| v[:length].to_i }
+i = 0
 
-set_V = Union_Find.new(lines.length - 1)
+edges.sort_by { |v| v[:length].to_i }
 
 min_tree = []
 
