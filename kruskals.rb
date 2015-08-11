e81e4a62d0534e7202745acdc4283eede4fb14e8
Isaac Archer <archeri@cat.pdx.edu>
2015-08-11 14:21:41 -0700

    update

--- kruskals.rb
+++ kruskals.rb
@@ -57,9 +57,9 @@ class Union_Find
       end
 end
 
-def check_reverse(v_a, v_b, edges, min_tree, edge)
-      edges.each do |v|
-            if ((v[:start] == v_a and v[:end] == v_b) or (v[:end] == v_a and v[:start] == v_b)) and min_tree.include? edge
+def check_reverse(v_a, v_b, min_tree)
+      min_tree.each do |v|
+            if ((v[:start] == v_a and v[:end] == v_b) or (v[:end] == v_a and v[:start] == v_b))
                   return true
             end
       end
@@ -104,7 +104,7 @@ min_tree = []
 
 edges.each do |edge|
       if set_V.connected(edge[:start], edge[:end])
-            if !check_reverse(edge[:start], edge[:end], edges, min_tree, edge)
+            if !check_reverse(edge[:start], edge[:end], min_tree)
                   min_tree.push(edge)
             end
             set_V.union(edge[:start], edge[:end])
