c6299518243e573d6a7eb64fd818d5c5178473c5
Isaac Archer <archeri@cat.pdx.edu>
2015-08-11 03:03:02 -0700

    update

--- kruskals.rb
+++ kruskals.rb
@@ -23,6 +23,8 @@ file = File.readlines("city-pairs.txt")
 
 lines = file.map { |fr| fr.split("\n").map(&:to_s) }
 
+p lines
+
 #edges = lines.each.map { |line| line.split(" ").map(&:to_s) }.map { |start_p, end_p, length| { :start_p => from, :end_p => to, :length => length } }.sort_by { |v| v[:length].to_i }
 
 edges = []
